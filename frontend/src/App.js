bd79407bfab187bcc8dc51e685d01c74b7006256
Francisco Ríos <frios@deloitte.es>
2021-07-05 18:25:26 +0200

    feat(FE_Walking_Skeleton): :zap: Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import data from './data.js';
+import Product from './components/Product.js';
 
 function App() {
   return (
@@ -19,38 +20,7 @@ function App() {
         <div>
           <div className="row center">
             {data.products.map(product => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">{product.price}€</div>
-                </div>
-              </div>
+              <Product key={product._id} product={product}></Product>
             ))}
           </div>
         </div>
