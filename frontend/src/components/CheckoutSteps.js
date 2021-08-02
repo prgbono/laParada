2d26067a749968e574243069d638c341f8c1ad0c
Francisco Ríos <frios@deloitte.es>
2021-08-02 18:03:54 +0200

    refactor(PAYMENT_&_ORDERS): :recycle: Change order of checkout steps

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -4,8 +4,8 @@ export default function CheckoutSteps(props) {
   return (
     <div className="row checkout-steps">
       <div className={props.step1 ? 'active' : ''}> Envío</div>
-      <div className={props.step2 ? 'active' : ''}> Pago</div>
-      <div className={props.step3 ? 'active' : ''}> Resumen</div>
+      <div className={props.step2 ? 'active' : ''}> Resumen</div>
+      <div className={props.step3 ? 'active' : ''}> Pago</div>
     </div>
   );
 }
