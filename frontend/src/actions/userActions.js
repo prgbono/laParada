df1acea4974faf57eecfa17a01c978b895097d67
Francisco Ríos <frios@deloitte.es>
2021-08-03 12:38:06 +0200

    perf(PAYMENT_&_ORDERS): :zap: Improve sign out process. Remove more user data on LS when Signing out

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -25,5 +25,7 @@ export const login = (email, password) => async dispatch => {
 
 export const signout = () => dispatch => {
   localStorage.removeItem('userInfo');
+  localStorage.removeItem('cartItems');
+  localStorage.removeItem('shippingAddress');
   dispatch({ type: USER_SIGNOUT });
 };
