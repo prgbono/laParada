ad9654c6715282b759726f7409a4cc7582223228
Francisco Ríos <frios@deloitte.es>
2021-08-05 12:39:49 +0200

    feat(AUTH): :sparkles: Method for checking valid authorization token

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,22 @@ export const generateToken = user => {
     { expiresIn: '14d' },
   );
 };
+
+export const isJWTAuth = (req, res, next) => {
+  // Pick token up from headers, url or body
+  const authorization =
+    req.get('Authorization') || req.query.token || req.body.token;
+
+  if (!authorization)
+    res.status(401).send({ message: 'No Authorization Token' });
+  else {
+    jwt.verify(authorization, process.env.JWT_SECRET, (err, payload) => {
+      if (err) res.status(401).send({ message: 'No Authorization Token' });
+      else {
+        // send userId to the next middleware which probably will need it
+        req.user = payload._id;
+        next();
+      }
+    });
+  }
+};
