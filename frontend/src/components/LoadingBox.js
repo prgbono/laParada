fc28ec3f499b7f289102b123f7a7757d2fc94a39
Francisco Ríos <frios@deloitte.es>
2021-08-21 11:19:47 +0200

    style(FE_React): :lipstick: Loading message style

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -2,5 +2,9 @@ import React from 'react';
 
 export default function LoadingBox() {
   //TODO: MaterialUI/AntD Components
-  return <i className="fa fa-spinner fa-spin"></i>;
+  return (
+    <div className="loading">
+      <i className="fa fa-spinner fa-spin"></i>;
+    </div>
+  );
 }
